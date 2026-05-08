6725f3053f9ed65a03f68ca25500fef9565120c2
iposiemo <paulnyabaro1@gmail.com>
2026-05-08 06:41:47 +0300

    Working with lists

--- list/list.py
+++ list/list.py
@@ -1,4 +1,11 @@
 friends = ['Jane', 'lilian', 'Ken', 'Rodgers']
 
+friends.append('Tony')
+friends.insert(2, 'Montana')
+del friends[1]
+
 for friend in friends:
     print("Hi my friend " + friend.title())
+
+latest_friend = friends.pop()
+print("My recently acquired friend is " + latest_friend)
